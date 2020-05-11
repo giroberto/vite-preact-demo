328bbfdf597f7aba0a310e90f5d97803e8c3da04
giroberto <umbertobarros@gmail.com>
2020-05-11 17:26:42 -0300

    Add environment variables

--- environment.ts
+++ environment.ts
@@ -1,3 +1,5 @@
+import devConfig from "./environment_dev";
+
 interface Config {
   TMDB_BASE_URL: string;
   TMDB_IMAGE_URL: string;
@@ -5,11 +7,14 @@ interface Config {
   TMDB_TOKEN: string;
 }
 
+const key = process.env.TMDB_KEY as string || devConfig.TMDB_KEY
+const token = process.env.TMDB_TOKEN as string || devConfig.TMDB_TOKEN
+
 const config: Config = {
   TMDB_BASE_URL: "https://api.themoviedb.org/3/",
   TMDB_IMAGE_URL: "https://image.tmdb.org/t/p/",
-  TMDB_KEY: "",
-  TMDB_TOKEN: "",
+  TMDB_KEY: key,
+  TMDB_TOKEN: token,
 };
 
 export default config;
---
+++ environment_dev.ts
@@ -0,0 +1,12 @@
+interface DevConfig {
+  TMDB_KEY: string;
+  TMDB_TOKEN: string;
+}
+
+const devConfig: DevConfig = {
+  TMDB_KEY: "b9ebfd5a43708505ae8bd6daac1e8115",
+  TMDB_TOKEN:
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiOWViZmQ1YTQzNzA4NTA1YWU4YmQ2ZGFhYzFlODExNSIsInN1YiI6IjVlN2Y4YTkxNDJmMTlmMDAxOGE1MjcxMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pPeJPHIrEOSGzjrgCu6s_L5t3JyJ7-nYe8DqitIg6TE",
+};
+
+export default devConfig;
